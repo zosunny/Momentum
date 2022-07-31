ff72dc1892f050f7fc2ff38ba31d7b823f63d134
zomochii <zoheyy108@naver.com>
2022-08-01 01:03:32 +0900

    array commit

---
+++ BeforeProject/array.js
@@ -0,0 +1,13 @@
+// array의 목적은 하나의 변수안에 데이터의 리스트를 가지는 것!
+
+const daysOfWeek = ["mon", "tue", "sed", "thu", "fri", "sat"];
+
+console.log(daysOfWeek);
+
+// Get Item from Array
+console.log(daysOfWeek[5]);
+
+// Add one more day to the array
+daysOfWeek.push("sun")
+
+console.log(daysOfWeek)
