e61fd9b61b841e049d3394e0bd6a92fbc86ac74e
zomochii <zoheyy108@naver.com>
2022-08-01 00:44:08 +0900

    booleans commit

---
+++ BeforeProject/booleans.js
@@ -0,0 +1,6 @@
+const amIFat = true
+const areYouFat = null;  /* 값이 없음 - 절대 자연발생적으로 생성 X */
+let somthing;
+console.log(areYouFat)
+console.log(amIFat)
+console.log(somthing)   /* undefined 출력 - 공간(메모리)은 존재하나 값이 없음 */
