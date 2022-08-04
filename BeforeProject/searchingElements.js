2caa476c745647a395b5f50e662c3dbef05736fc
zomochii <zoheyy108@naver.com>
2022-08-05 01:11:54 +0900

    searching for elements commit

---
+++ BeforeProject/searchingElements.js
@@ -0,0 +1,28 @@
+document.h1 = "Hello From Js"  // JS로 HTML 정보를 갖고 있는 document 객체 이용해 HTML 변경
+
+const h1 = document.getElementById("h1");
+
+console.dir(h1);
+
+h1.innerText = "Got you!";
+
+console.log(h1.id);          // h1
+console.log(h1.className);   // 지정되어있지 않으면 빈 칸 나옴
+
+const hellos = document.getElementsByClassName("hello");   // class = "hello"가 여러개 있으면 배열을 가져다 줌
+console.log(hellos);
+
+const h1 = document.querySelector(".hello h1");  // querySelector는 element를 css방식으로 검색할 수 있음
+                      // querySelector에서는 hello가 class name이라는 것과 그 안의 h1을 명시해주어야 한다.
+const title2 = document.querySelectorAll(".hello h1:fist-child")
+                      // querySelectorAll은 selector 안의 조건에 부합하는 모든 element를 가져다 줌
+const id1 = document.querySelector("#hello");   // 같은 명령
+const id2 = document.getElementById("hello");   // 같은 명령
+
+console.log(h1)
+
+const h1 = document.querySelector("div.hello:first-child h1");
+
+console.dir(h1)
+
+h1.style.color = "blue";
