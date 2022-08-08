f6819d966c23b57e67fe8ee0c02e73e3f7306c34
zomochii <zoheyy108@naver.com>
2022-08-09 01:48:11 +0900

    todo -ing commit

--- js/todo.js
+++ js/todo.js
@@ -2,19 +2,36 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos(){
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDo(e){
+  const li = e.target.parentElement;  //target = HTML element 여기서 target은 button이고 parentElemet는 button의 li
+  li.remove();                        //parentElement = HTML element의 속성들 중 하나로 클릭된 element의 부모를 가리킴
+}
+
 function paintToDo(newTodo){
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);     //<li><span></span></li> 로 만들어줌
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);     //<li><span></span></li> 로 만들어줌
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(e){
   e.preventDefault();
   const newTodo = toDoInput.value;    // 값 저장하고
-  toDoInput.value = "";               // 비움  
+  toDoInput.value = "";               // 비움
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
